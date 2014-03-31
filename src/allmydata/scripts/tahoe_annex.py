f86749af658735fc2f1d8db69fa57c9c0a388aef
Alberto Berti <alberto@metapensiero.it>
2014-03-31 20:39:06 +0200

    Add initial draft version of annex command

---
+++ src/allmydata/scripts/tahoe_annex.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# :Progetto:  tahoe-lafs -- git annex command
+# :Creato:    lun 31 mar 2014 18:42:03 CEST
+# :Autore:    Alberto Berti <alberto@metapensiero.it>
+# :Licenza:   GNU General Public License version 3 or later
+#
+
+from twisted.internet import stdio, reactor
+from twisted.protocols import basic
+#from twisted.web import client
+
+
+class AnnexServerProtocol(basic.LineReceiver):
+
+    delimiter = '\n'
+
+    def connectionMade(self):
+        self.sendLine('VERSION 1')
+
+
+def annex(stdin=0, stdout=1):
+    return stdio.StandardIO(AnnexServerProtocol(), stdin, stdout)
+
+
+if __name__ == "__main__":
+    stdio.StandardIO(AnnexServerProtocol())
+    reactor.run()
---
+++ src/allmydata/test/test_annex.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# :Progetto:  allmydata-tahoe -- tahoe annex test case
+# :Creato:    lun 31 mar 2014 18:17:30 CEST
+# :Autore:    Alberto Berti <alberto@metapensiero.it>
+# :Licenza:   GNU General Public License version 3 or later
+#
+
+from twisted.trial import unittest
+from twisted.test import proto_helpers
+
+from allmydata.scripts import tahoe_annex
+
+
+class AnnexTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.proto = tahoe_annex.AnnexServerProtocol()
+        self.tr = proto_helpers.StringTransport()
+
+    def test_version(self):
+        self.proto.makeConnection(self.tr)
+        self.assertEqual(self.tr.value(), 'VERSION 1\n')
